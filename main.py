75490f3b11f0edfc6b1008e0d5512cd2f14864ac
Rahul Jain <rahuljain809@gmail.com>
2023-09-06 15:22:18 -0700

    tocr commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import torch
 import os
 import glob
 
-device = torch.device('cuda:0' if torch.cuda.is_available else 'cpu')
+device = 'cpu'# torch.device('cuda:0' if torch.cuda.is_available else 'cpu')
 
 def download_and_unzip(url, save_path):
     print(f"Downloading and extracting assets....", end="")
@@ -31,3 +31,52 @@ asset_zip_path = os.path.join(os.getcwd(), "images.zip")
 # Download if assest ZIP does not exists.
 if not os.path.exists(asset_zip_path):
     download_and_unzip(URL, asset_zip_path)
+
+def read_image(image_path):
+    """
+    :param image_path: String, path to the input image.
+    Returns:
+        image: PIL Image.
+    """
+    image = Image.open(image_path).convert('RGB')
+    return image
+
+def ocr(image, processor, model):
+    """
+    :param image: PIL Image.
+    :param processor: Huggingface OCR processor.
+    :param model: Huggingface OCR model.
+
+    Returns:
+        generated_text: the OCR'd text string.
+    """
+    # We can directly perform OCR on cropped images.
+    pixel_values = processor(image, return_tensors='pt').pixel_values.to(device)
+    generated_ids = model.generate(pixel_values)
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    return generated_text
+
+
+def eval_new_data(data_path=None, num_samples=4, model=None):
+    image_paths = glob.glob(data_path)
+    for i, image_path in tqdm(enumerate(image_paths), total=len(image_paths)):
+        if i == num_samples:
+            break
+        image = read_image(image_path)
+        text = ocr(image, processor, model)
+        plt.figure(figsize=(7, 4))
+        plt.imshow(image)
+        plt.title(text)
+        plt.axis('off')
+        plt.show()
+
+processor = TrOCRProcessor.from_pretrained('microsoft/trocr-small-printed')
+model = VisionEncoderDecoderModel.from_pretrained(
+    'microsoft/trocr-small-printed'
+).to(device)
+
+eval_new_data(
+    data_path=os.path.join('images', 'newspaper', '*'),
+    num_samples=2,
+    model=model
+)
